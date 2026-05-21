4e0f1a64e363de37458d4a9db62f52d584cb17a5
optiumpodologia <podologiaoptium@gmail.com>
2026-05-21 14:37:16 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Configuración del proyecto Supabase
-// Si en algún momento cambia el proyecto, solo hay que actualizar estos dos valores
-const SUPABASE_URL = 'https://cyxqsczibpvwuqprhuim.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_F5Ia6C9oKBkgevCSpPjcCw_qhUS5bHo';
